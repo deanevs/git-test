1ef67475ba5d848ba750155cabcc4c9e430c4f8f
deanevs <evs_bud@hotmail.com>
2024-08-19 11:22:22 +0100

    first commit with main

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ def main():
 def branched():
     print("Commit 2")
 
+def branched_further():
+    print("Branched")
+
 if __name__ == '__main__':
     main()
     branched()
+    branched_further()
 
 
